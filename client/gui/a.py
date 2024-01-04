797bca18b96947445c191c1412740a2ee652fc99
woshilapp <2384138985@qq.com>
2024-01-04 23:13:52 +0800

    gui

--- client/gui/a.py
+++ client/gui/a.py
@@ -24,6 +24,13 @@ def on_select(event):
 root = tk.Tk()
 root.title("Listbox with Text Example")
 
+menubar = tk.Menu(root).pack()        # 建立最上层菜单
+# 建立菜单类别对象，并将此菜单类别命名为File
+filemenu = tk.Menu(menubar,tearoff=False)
+menubar.add_cascade(label="File",menu=filemenu)
+# 在File菜单内建立菜单列表Exit
+filemenu.add_command(label="Exit",command=root.destroy)
+
 # 创建一个Listbox
 listbox = tk.Listbox(root)
 listbox.pack(side=tk.LEFT, padx=10, pady=10)
@@ -38,6 +45,8 @@ listbox.bind("<<ListboxSelect>>", on_select)
 # 创建一个Text组件
 text = tk.Text(root, width=30, height=10)
 text.pack(side=tk.LEFT, padx=10, pady=10)
+
+root.config(menu=menubar)
 
 # 运行主循环
 root.mainloop()
